2282096817f2592fb4c3ec89bb49e21cf94a495e
Ilya-ren <m.zorricueta@duocuc.cl>
2024-01-10 21:15:30 -0300

    Funcion resta

---
+++ js/resta.js
@@ -0,0 +1,12 @@
+
+    function resta (){
+        var v1= parseInt(document.getElementById("valor1").value);
+        var v2= parseInt(document.getElementById("valor2").value);
+        var resta=0;
+
+        resta= v1-v2;
+
+        document.getElementById("resultado").value = resta;
+
+        
+    }
